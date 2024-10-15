384f52b17edad9253ca843271bb829d295735833
Davi Alpiano <davialpiano@gmail.com>
2024-10-15 17:09:06 -0300

    Req 6

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,14 +2,18 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -40,5 +44,23 @@ public class MuseumController {
   public ResponseEntity<Museum> createMuseum(@RequestBody Museum newMuseum) {
     Museum museum = museumService.createMuseum(newMuseum);
     return ResponseEntity.status(HttpStatus.CREATED).body(museum);
+  }
+
+  /**
+   * Closest museum response entity.
+   *
+   * @param lat         the lat
+   * @param lng         the lng
+   * @param maxDistKm the max dist km
+   * @return the response entity
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> closestMuseum(@RequestParam
+      String lat, String lng, Double maxDistKm
+  ) {
+    Coordinate coordinate = new Coordinate(Double.parseDouble(lat), Double.parseDouble(lng));
+    Museum museum = museumService.getClosestMuseum(coordinate, maxDistKm);
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(museum);
+    return ResponseEntity.status(HttpStatus.OK).body(museumDto);
   }
 }
